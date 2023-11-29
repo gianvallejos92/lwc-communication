984eaef2b36297cc16d8fa00ff91156c47de1bb2
Gian Vallejos <gian.vallejos92@gmail.com>
2023-11-28 20:43:32 -0500

    message channel from child to unrelated component

--- force-app/main/default/lwc/hijoComponente/hijoComponente.js
+++ force-app/main/default/lwc/hijoComponente/hijoComponente.js
@@ -1,8 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EJEMPLO_LEX from '@salesforce/messageChannel/example__c';
 
 export default class HijoComponente extends LightningElement {
   message = "Soy un componente Hijo";
   textoHijo = "";
+
+  @wire(MessageContext)
+  messageContext;
 
   handleTextoHijo (event) {
     this.textoHijo = event.target.value;
@@ -10,6 +15,17 @@ export default class HijoComponente extends LightningElement {
 
   enviarAPadre () {
     console.log('Texto Hijo: ' + this.textoHijo);
+    this.textoHijo = "";
+  }
+
+  enviarAOtro () {
+    console.log('Publish Message Channel');
+    const payload = {
+      message: this.textoHijo
+    };
+
+    publish(this.messageContext, EJEMPLO_LEX, payload);
+
     this.textoHijo = "";
   }
 
--- force-app/main/default/lwc/otroComponente/otroComponente.js
+++ force-app/main/default/lwc/otroComponente/otroComponente.js
@@ -1,8 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import EJEMPLO_LEX from '@salesforce/messageChannel/example__c';
 
 export default class OtroComponente extends LightningElement {
   message = "Soy un componente no relacionado";
   textoOtro = "";
+
+  subscription = null;
+  @wire(MessageContext)
+  messageContext;
+
+  connectedCallback () {
+    this.subscription = subscribe(
+      this.messageContext, 
+      EJEMPLO_LEX, 
+      (payload) => this.handlePayload(payload)
+    );
+  }
+
+  handlePayload (payload) {
+    this.message = payload.message;
+  }
 
   handleTextoOtro (event) {
     this.textoOtro = event.target.value;
